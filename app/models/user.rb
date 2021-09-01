adc7593ac80f900e3419c5915b5d29ebe7bb106e
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-01 11:22:11 +0900

    userモデルファイルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # パスワードを英数字混合に制限します。不要であれば削除してください。
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英字と数字の両方を含む必要があります'
+
+  validates :nickname, presence: true
+  has_many :meals, dependent: :destroy
+  has_many :tags, dependent: :destroy
+  has_many :logs, dependent: :destroy
 end
