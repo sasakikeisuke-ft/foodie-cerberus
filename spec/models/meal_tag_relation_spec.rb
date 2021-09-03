4fa60859743e1cac7b4c1bd5ec5821c78125a542
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-03 22:11:12 +0900

    Rubocopを実行

--- spec/models/meal_tag_relation_spec.rb
+++ spec/models/meal_tag_relation_spec.rb
@@ -17,24 +17,26 @@ RSpec.describe MealTagRelation, type: :model do
       it 'mealが紐づけられていないと登録できない' do
         @meal_tag_relation.meal = nil
         @meal_tag_relation.valid?
-        expect(@meal_tag_relation.errors.full_messages).to include("Mealを入力してください")
+        expect(@meal_tag_relation.errors.full_messages).to include('Mealを入力してください')
       end
       it 'meal_idの重複があり登録できない' do
         @meal_tag_relation.save
-        another_meal_tag_relation = FactoryBot.build(:meal_tag_relation, meal_id: @meal_tag_relation.meal_id, tag: @meal_tag_relation.tag)
+        another_meal_tag_relation = FactoryBot.build(:meal_tag_relation, meal_id: @meal_tag_relation.meal_id,
+                                                                         tag: @meal_tag_relation.tag)
         another_meal_tag_relation.valid?
-        expect(another_meal_tag_relation.errors.full_messages).to include("Mealはすでに存在します")
+        expect(another_meal_tag_relation.errors.full_messages).to include('Mealはすでに存在します')
       end
       it 'tagが紐づけられていないと登録できない' do
         @meal_tag_relation.tag = nil
         @meal_tag_relation.valid?
-        expect(@meal_tag_relation.errors.full_messages).to include("Tagを入力してください")
+        expect(@meal_tag_relation.errors.full_messages).to include('Tagを入力してください')
       end
       it 'tag_idの重複があり登録できない' do
         @meal_tag_relation.save
-        another_meal_tag_relation = FactoryBot.build(:meal_tag_relation, tag_id: @meal_tag_relation.tag_id, meal: @meal_tag_relation.meal)
+        another_meal_tag_relation = FactoryBot.build(:meal_tag_relation, tag_id: @meal_tag_relation.tag_id,
+                                                                         meal: @meal_tag_relation.meal)
         another_meal_tag_relation.valid?
-        expect(another_meal_tag_relation.errors.full_messages).to include("Tagはすでに存在します")
+        expect(another_meal_tag_relation.errors.full_messages).to include('Tagはすでに存在します')
       end
     end
   end
