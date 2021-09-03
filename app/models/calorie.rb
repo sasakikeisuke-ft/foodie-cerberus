3757a9d78a57cab9a2050d71f85f344d0066cbe9
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-03 21:27:14 +0900

    ActiveHash: calorieを作成

---
+++ app/models/calorie.rb
@@ -0,0 +1,10 @@
+class Calorie < ActiveHash::Base
+  self.data = [
+    { id: 1, select: '低い' },
+    { id: 2, select: '普通' },
+    { id: 3, select: '高い' },
+    { id: 4, select: '選択せず登録' }
+  ]
+    include ActiveHash::Associations
+  has_many :meals
+end
