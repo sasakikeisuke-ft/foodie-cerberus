a167c7b96ca8e38fcecf034e683c7d720d1ebc33
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-03 21:54:36 +0900

    ActiveHash: categoryを作成

---
+++ app/models/category.rb
@@ -0,0 +1,16 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '----', name_ja: '----' },
+    { id: 1, name: 'meat', name_ja: 'お肉' },
+    { id: 2, name: 'fish', name_ja: 'お魚' },
+    { id: 3, name: 'vegetable', name_ja: '野菜' },
+    { id: 4, name: 'rice', name_ja: '米・穀物' },
+    { id: 5, name: 'noodles', name_ja: '麺類' },
+    { id: 5, name: 'eating-out', name_ja: '外食・' },
+    { id: 5, name: 'microwave', name_ja: '電子レンジ' },
+    { id: 5, name: 'retort_food', name_ja: 'レトルト' },
+    { id: 5, name: 'free', name_ja: 'フリー' },
+  ]
+  include ActiveHash::Associations
+  has_many :tags
+end
