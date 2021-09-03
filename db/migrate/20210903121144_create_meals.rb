76336c1f123846aeff67eb97174435e862358ae1
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-03 21:13:48 +0900

    mealsテーブルの作成
    マイグレーションも実行しました。

--- db/migrate/20210903121144_create_meals.rb
+++ db/migrate/20210903121144_create_meals.rb
@@ -1,7 +1,12 @@
 class CreateMeals < ActiveRecord::Migration[6.0]
   def change
     create_table :meals do |t|
-
+      t.string :name, null: false
+      t.date :last_day, null: false
+      t.integer :price_id, null: false
+      t.integer :calorie_id, null: false
+      t.references :user, foreign_key: true
+      t.text :link
       t.timestamps
     end
   end
