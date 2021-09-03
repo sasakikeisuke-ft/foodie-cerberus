7936f6539aa0778a805297f8bac0164a4183b955
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-03 22:09:04 +0900

    meal_tag_relationモデルファイルの編集

--- app/models/meal_tag_relation.rb
+++ app/models/meal_tag_relation.rb
@@ -1,2 +1,6 @@
 class MealTagRelation < ApplicationRecord
+  validates :meal, uniqueness: { case_sensitive: false, scope: :tag_id }
+  validates :tag, uniqueness: { case_sensitive: false, scope: :meal_id }
+  belongs_to :meal
+  belongs_to :tag
 end
