6ab1371d493ef9fc4904293d9cbcad12005e0fc2
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-03 21:14:44 +0900

    mealモデルファイルを編集

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,19 @@
 class Meal < ApplicationRecord
+  with_options presence: true do
+    with_options numericality: { other_than: 0, message: "を選択してください" } do
+      validates :price_id
+      validates :calorie_id
+    end
+    validates :name, uniqueness: { case_sensitive: false, scope: :user_id }
+    validates :last_day
+  end
+  belongs_to :user
+  has_many :meal_tag_relations, dependent: :destroy
+  has_many :tags, through: :meal_tag_relations
+  has_many :logs, dependent: :destroy
+
+  # ActiveHash
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :price
+  belongs_to :calorie
 end
