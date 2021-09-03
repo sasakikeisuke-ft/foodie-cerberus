bb2945dae0e0e5fab544abeb9ecfed80b006cc75
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-03 22:31:25 +0900

    mealsコントローラーを編集

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,58 @@
 class MealsController < ApplicationController
+  before_action :meal_form_variable, only: [:new, :edit]
+  before_action :common_variable1, only: [:edit, :update, :destroy, :show]
+
+  def index
+    @meals = Meal.where(user_id: current_user.id)
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to root_path
+    else
+      meal_form_variable
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @meal.update(meal_params)
+      redirect_to root_path
+    else
+      meal_form_variable
+      render :edit
+    end
+  end
+
+  def destroy
+    @meal.destroy
+    redirect_to root_path
+  end
+
+  def show
+    @tags = Tag.where(user_id: current_user.id)
+    @meal_tag_relations = MealTagRelation.where(meal_id: params[:meal_id])
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :last_day, :link, :price_id, :calorie_id).merge(user_id: current_user.id)
+  end
+
+  def common_variable1
+    @meal = Meal.find(params[:id])
+  end
+
+  def meal_form_variable
+  end
+
 end
