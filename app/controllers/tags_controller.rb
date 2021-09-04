65ec303e4d17012ff9b74d96f9451bbba3b94817
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-04 12:34:56 +0900

    tagsコントローラーファイルの仮作成

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,60 @@
 class TagsController < ApplicationController
+  before_action :tag_form_variable, only: [:new, :edit]
+  before_action :common_variable1, only: [:edit, :update, :destroy, :show]
+
+  def index
+    @tags = Tag.where(user_id: current_user.id)
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to root_path
+    else
+
+      tag_form_variable
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tag.update(tag_params)
+      redirect_to root_path
+    else
+
+      tag_form_variable
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag.destroy
+    redirect_to root_path
+  end
+
+  def show
+    @meals = Meal.where(user_id: current_user.id)
+    @meal_tag_relations = MealTagRelation.where(tag_id: params[:tag_id])
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name, :category_id).merge(user_id: current_user.id)
+  end
+
+  def common_variable1
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_form_variable
+  end
+
 end
