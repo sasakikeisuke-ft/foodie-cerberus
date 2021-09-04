22c721eea2f424741eb4f505c6e7f4d7aa7a9a11
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-04 16:10:49 +0900

    料理詳細ページから、登録済みのタグを登録できる機能を実装

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,6 @@
 class MealsController < ApplicationController
   before_action :meal_form_variable, only: [:new, :edit]
-  before_action :common_variable1, only: [:edit, :update, :destroy, :show]
+  before_action :common_variable1, only: [:edit, :update, :destroy]
 
   def index
     @meals = Meal.where(user_id: current_user.id).order(:last_day)
@@ -38,8 +38,17 @@ class MealsController < ApplicationController
   end
 
   def show
-    @tags = Tag.where(user_id: current_user.id)
-    @meal_tag_relations = MealTagRelation.where(meal_id: params[:meal_id])
+    common_variable2
+  end
+
+  def add_tag
+    meal_tag_relation = MealTagRelation.new(meal_id: params[:id], tag_id: params[:tag_id])
+    if meal_tag_relation.save
+      redirect_to meal_path(params[:id])
+    else
+      common_variable2
+      render :show
+    end
   end
 
   private
@@ -50,6 +59,12 @@ class MealsController < ApplicationController
 
   def common_variable1
     @meal = Meal.find(params[:id])
+  end
+
+  def common_variable2
+    @meal = Meal.includes(:tags).find(params[:id])
+    not_target_tags = MealTagRelation.where(meal_id: params[:id]).select(:tag_id)
+    @tags = Tag.where(user_id: current_user.id).where.not(id: not_target_tags)
   end
 
   def meal_form_variable
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'tests/index'
   devise_for :users
   root to: 'tests#index'
-  resources :meals
+  resources :meals do
+    member do
+      post 'add_tag'
+    end
+  end
   resources :tags
   resources :logs, only: [:index, :create, :update, :destroy]
 end
