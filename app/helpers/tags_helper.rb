5b2e37008562134581eaacd9d9ab95b249b7da03
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-05 10:19:42 +0900

    タグ一覧画面の別バーションを作成
    タグ一覧画面をテーブルではなく、カテゴリーごとに表示する場合のものを作成しました。
    現在は予備2として保存しています。
    今後どちらを採用するか検討していきます。

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,19 @@
 module TagsHelper
+  # @tagsをcategoryごとに分類しハッシュで返すメソッド
+  def distribution_tags(tags)
+    contents = make_tag_contents_base
+    tags.each do |tag|
+      contents[tag.category.name.to_sym] << tag
+    end
+    contents
+  end
+
+  def make_tag_contents_base
+    contents = {category_index: Category.where.not(id: 0)}
+    contents[:category_index].each do |category|
+      contents[category.name.to_sym] = []
+    end
+    contents
+  end
+
 end
