8b6f3fcd8a957f5b61d592bdb5a0c2c94c2ed851
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-05 23:55:57 +0900

    献立提案機能から、献立を登録できる機能を実装
    献立の提案から、料理を献立に追加できるようにしました。
    献立提案のデザインが未決定のため、機能のみの追加としています。

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,11 +1,17 @@
 class LogsController < ApplicationController
+  before_action :common_variable1, only: [:index]
 
   def index
-    @logs = Log.includes(meal: :tags).where(user_id: current_user.id)
-    @meals = Meal.includes(:tags).where(user_id: current_user.id).where.not(id: @logs.select(:meal_id)).order(:last_day)
   end
 
   def create
+    log = Log.new(meal_id: params[:id], user_id: current_user.id )
+    if log.save
+      redirect_to logs_path
+    else
+      common_variable1
+      render :index
+    end
   end
 
   def update
@@ -17,6 +23,11 @@ class LogsController < ApplicationController
   end
 
   private
+
+  def common_variable1
+    @logs = Log.includes(meal: :tags).where(user_id: current_user.id)
+    @meals = Meal.includes(:tags).where(user_id: current_user.id).where.not(id: @logs.select(:meal_id)).order(:last_day)
+  end
 
 end
 
