20495fe355c55f578e8b424a3c2fbb6a28edaac1
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-05 15:38:18 +0900

    logsコントローラーの各種アクション仮置き

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,22 @@
 class LogsController < ApplicationController
+
+  def index
+    @logs = Log.where(user_id: current_user.id)
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+    @log.destroy
+    redirect_to root_path
+  end
+
+  private
+
 end
+
+# bundle exec rubocop -a
