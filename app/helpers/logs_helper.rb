a1a30a7cecbee5bc2390591451ae98c88ef50395
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-05 17:30:23 +0900

    献立一覧と関連タグ一覧を仮設

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,2 +1,15 @@
 module LogsHelper
+  def make_menu(logs)
+    contents = {meals: [], tags: []}
+    logs.each do |log|
+      contents[:meals] << log.meal
+      log.meal.tags.each do |tag|
+        contents[:tags] << tag
+      end
+    end
+    contents
+  end
+
+
+
 end
