2f6c23633c0435220ab3e106e6e6f46941031a16
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-05 23:40:26 +0900

    本日の献立ページに、献立の提案表示機能を追加
    本日の献立ページに、さまざまな条件から食事を提案する機能を実装しました。

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,6 +2,7 @@ class LogsController < ApplicationController
 
   def index
     @logs = Log.includes(meal: :tags).where(user_id: current_user.id)
+    @meals = Meal.includes(:tags).where(user_id: current_user.id).where.not(id: @logs.select(:meal_id)).order(:last_day)
   end
 
   def create
--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -11,6 +11,41 @@ module LogsHelper
     menu
   end
 
+  # 献立の提案を作成するメソッド
+  def make_suggestion(meals)
+    category_index = Category.where.not(id: 0)
+    oldest_meals = make_oldest_meals(meals)
+    suggestion = {
+      category_index: category_index,
+      random: meals[rand(0..meals.length - 1)].name,
+      oldest: oldest_meals[rand(0..oldest_meals.length - 1)].name
+    }
+
+    end_count = Category.where.not(id: 0).length
+    count = 0
+    meals.each do |meal|
+      break if count == end_count
+
+      meal.tags.each do |tag|
+        next unless suggestion[tag.category.name.to_sym].nil?
+
+        suggestion[tag.category.name.to_sym] = meal
+        count += 1
+      end
+    end
+    suggestion
+  end
+
+  def make_oldest_meals(meals)
+    oldest_meals = []
+    oldest_day = meals[0].last_day
+    meals.each do |meal|
+      break unless meal.last_day == oldest_day
+
+      oldest_meals << meal
+    end
+    oldest_meals
+  end
 
 
 end
