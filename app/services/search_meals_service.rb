c92b34fe9a860a5432953fc1e069d5afde87ac56
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-14 20:18:43 +0900

    価格で検索する機能を追加

--- app/services/search_meals_service.rb
+++ app/services/search_meals_service.rb
@@ -16,6 +16,17 @@ class SearchMealsService
     when 'price_expensive'
       meals = Meal.where(user_id: user_id, price_id: 3).order(:last_day)
       comment = '検索: 価格が高い'
+    when 'labor_little'
+      meals = Meal.where(user_id: user_id, labor_id: 1).order(:last_day)
+      comment = '検索: 手間はかからない'
+    when 'labor_normal'
+      meals = Meal.where(user_id: user_id, labor_id: 2).order(:last_day)
+      comment = '検索: 手間は普通'
+    when 'labor_lot'
+      meals = Meal.where(user_id: user_id, labor_id: 3).order(:last_day)
+      comment = '検索: 価格がかかる'
+    else # 名前で検索
+      puts '名前で検索まだ未実装'
     end
     content = {
       meals: meals,
