527ed2e7423b5e5640d5974d405928916c3c70a4
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-14 20:00:09 +0900

    各価格で検索できる機能を追加

--- app/services/search_meals_service.rb
+++ app/services/search_meals_service.rb
@@ -8,11 +8,14 @@ class SearchMealsService
       meals = Meal.where(user_id: user_id).order(last_day: "DESC")
       comment = '最近食べた順番で表示しています。'
     when 'price_cheap'
-
+      meals = Meal.where(user_id: user_id, price_id: 1).order(:last_day)
+      comment = '検索: 価格が安い'
     when 'price_normal'
-
+      meals = Meal.where(user_id: user_id, price_id: 2).order(:last_day)
+      comment = '検索: 価格は普通'
     when 'price_expensive'
-
+      meals = Meal.where(user_id: user_id, price_id: 3).order(:last_day)
+      comment = '検索: 価格が高い'
     end
     content = {
       meals: meals,
