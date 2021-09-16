3fdb08e762749c12d0f637c1ebee2073516eacbb
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-09-16 22:49:00 +0900

    検索用サービスクラスをリファクタリング
    選択肢をeach文でまとめれるように修正しました。
    またそれに伴い、本日の献立ページに記載した検索リンクのパスを編集しました。

--- app/services/search_meals_service.rb
+++ app/services/search_meals_service.rb
@@ -6,74 +6,35 @@ class SearchMealsService
         comment = '検索ワードが空欄でした。登録した順番に表示しています。'
        else
         meals = Meal.where(user_id: user_id).where('name LIKE(?)', "%#{name}%")
-        comment = "検索: 料理名 #{name}"
+        comment = "検索: #{name}を含む料理名"
       end
     else
-      case search
-      when 'last_day_asc'
-        meals = Meal.where(user_id: user_id).order(:last_day)
-        comment = '食べたのが古い順番で表示しています。'
-      when 'last_day_desc'
-        meals = Meal.where(user_id: user_id).order(last_day: "DESC")
-        comment = '食べたのが最近である順番で表示しています。'
-      when 'price_cheap'
-        meals = Meal.where(user_id: user_id, price_id: 1).order(:last_day)
-        comment = '検索: 価格が安い'
-      when 'price_normal'
-        meals = Meal.where(user_id: user_id, price_id: 2).order(:last_day)
-        comment = '検索: 価格は普通'
-      when 'price_expensive'
-        meals = Meal.where(user_id: user_id, price_id: 3).order(:last_day)
-        comment = '検索: 価格が高い'
-      when 'labor_little'
-        meals = Meal.where(user_id: user_id, labor_id: 1).order(:last_day)
-        comment = '検索: 手間はかからない'
-      when 'labor_normal'
-        meals = Meal.where(user_id: user_id, labor_id: 2).order(:last_day)
-        comment = '検索: 手間は普通'
-      when 'labor_lot'
-        meals = Meal.where(user_id: user_id, labor_id: 3).order(:last_day)
-        comment = '検索: 価格がかかる'
-      when 'calorie_row'
-        meals = Meal.where(user_id: user_id, calorie_id: 1).order(:last_day)
-        comment = '検索: カロリーが低い'
-      when 'calorie_normal'
-        meals = Meal.where(user_id: user_id, calorie_id: 2).order(:last_day)
-        comment = '検索: カロリーは普通'
-      when 'calorie_high'
-        meals = Meal.where(user_id: user_id, calorie_id: 3).order(:last_day)
-        comment = '検索: カロリーが高い'
-      when 'meat'
-        meals = Meal.where(user_id: user_id, category_id: 1).order(:last_day)
-        comment = '検索: お肉'
-      when 'fish'
-        meals = Meal.where(user_id: user_id, category_id: 2).order(:last_day)
-        comment = '検索: お魚'
-      when 'vegetable'
-        meals = Meal.where(user_id: user_id, category_id: 3).order(:last_day)
-        comment = '検索: 野菜'
-      when 'rice'
-        meals = Meal.where(user_id: user_id, category_id: 4).order(:last_day)
-        comment = '検索: 米・穀物'
-      when 'noodles'
-        meals = Meal.where(user_id: user_id, category_id: 5).order(:last_day)
-        comment = '検索: 麺類'
-      when 'eating_out'
-        meals = Meal.where(user_id: user_id, category_id: 6).order(:last_day)
-        comment = '検索: 外食'
-      when 'microwave'
-        meals = Meal.where(user_id: user_id, category_id: 7).order(:last_day)
-        comment = '検索: 電子レンジ'
-      when 'retort_food'
-        meals = Meal.where(user_id: user_id, category_id: 8).order(:last_day)
-        comment = '検索: レトルト'
-      when 'free'
-        meals = Meal.where(user_id: user_id, category_id: 9).order(:last_day)
-        comment = '検索: Free'
-      else
-        tag = Tag.where(user_id: user_id).find(search)
+      case search[:type]
+      when 'last_day'
+        meals = Meal.where(user_id: user_id).order(last_day: search[:id])
+        case search[:id]
+        when 'ASC'
+          comment = '最後に食べた日: 昔の順番'
+        when 'DESC'
+          comment = '最後に食べた日: 最近の順番'
+        end
+      when 'price'
+        meals = Meal.where(user_id: user_id, price_id: search[:id]).order(:last_day)
+        comment = "価格: #{Price.find(search[:id]).select}"
+      when 'labor'
+        meals = Meal.where(user_id: user_id, labor_id: search[:id]).order(:last_day)
+        comment = "大変さ: #{Labor.find(search[:id]).select}"
+      when 'calorie'
+        meals = Meal.where(user_id: user_id, calorie_id: search[:id]).order(:last_day)
+        comment = "カロリー: #{Calorie.find(search[:id]).select}"
+      when 'category'
+        meals = Meal.where(user_id: user_id, category_id: search[:id]).order(:last_day)
+        comment = "カテゴリー: #{Category.find(search[:id]).name_ja}"
+      when 'tag'
+        tag = Tag.where(user_id: user_id).find(search[:id])
         meals = tag.meals
-      end  
+        comment = "タグ: #{tag.name}"
+      end
     end
     content = {
       meals: meals,
